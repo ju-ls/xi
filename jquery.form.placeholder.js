b597c8b2286ef09c4b9f582ded5c1af121fc5995
ju-ls <xi@o-o.rf.gd>
2016-11-11 02:27:44 -0500

    added the old 404 page files

---
+++ jquery.form.placeholder.js
@@ -0,0 +1,24 @@
+jQuery(document).ready(function($)
+{
+	$("input[data-default], textarea[data-default]").each(function()
+	{
+		$(this).val($(this).attr("data-default"));
+	});
+	
+	$("input[data-default], textarea[data-default]").focus(function()
+	{
+		if($(this).val() == $(this).attr("data-default"))
+		{
+			$(this).val("");
+		}
+	});
+	
+	$("input[data-default], textarea[data-default]").blur(function()
+	{
+		if($(this).val() == "")
+		{
+			$(this).val($(this).attr("data-default"));
+		}
+	});
+	
+});
